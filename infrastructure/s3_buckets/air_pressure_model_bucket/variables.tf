b25e2e964e81e004e7208b30bdba71ca2b76dfbd
sethusaim <sethusaim@gmail.com>
2022-08-29 13:44:18 +0530

    added model bucket

---
+++ infrastructure/s3_buckets/air_pressure_model_bucket/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "model" {
+  type    = string
+  default = "air-pressure-model"
+}
+
+variable "aws_account_id" {
+  type    = string
+  default = "347460842118"
+}
+
+variable "force_destroy_bucket" {
+  type    = bool
+  default = true
+}
